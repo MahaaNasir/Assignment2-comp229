c1911eb24751db92429ea4ea043b3b415a7aa126
MahaaNasir <mahanasir25392@gmail.com>
2023-06-18 16:30:07 -0400

    updated db URL to work with MOngoDB Atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,6 @@
 module.exports = 
 {
     //databse with mongosh
-    "URI":"mongodb://localhost/user_collection"
+    //"URI":"mongodb://localhost/user_collection"
+    "URI":"mongodb+srv://maha:3mYPRSRnCiGjx89g@mongodbserver.20af57j.mongodb.net/?retryWrites=true&w=majority"
 }
